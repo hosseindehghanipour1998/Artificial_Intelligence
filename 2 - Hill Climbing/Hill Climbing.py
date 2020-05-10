16c4201237c0cb3ae685b011da693467a75db86c
Hossein <hossein.dehghanipour1998@gmail.com>
2020-05-10 08:02:10 +0430

    Hill Climbing
    Plot Function Implemented.
    Function tested and it was OK :+1:

--- 2 - Hill Climbing/Hill Climbing.py
+++ 2 - Hill Climbing/Hill Climbing.py
@@ -1,6 +1,9 @@
 '''
 Before Running the code you should install "sympy" by using this command :
     > pip install sympy
+
+I order to use "matplot" to plot the curve :
+    > pip install matplotlib
 '''
 
 
@@ -18,6 +21,7 @@ x , y = sp.symbols('x y')
 import sympy as sp
 from random import random
 from Writer import Writer
+import matplotlib.pyplot as plt
 ############################
 # Initializing Symbols
 pi = sp.pi 
@@ -27,7 +31,8 @@ x , y = sp.symbols('x y')
 f = (sp.sin(10*sp.pi*x)/(2*x)) + (x-1)**4 # change this function based on our own demand.
 lowerBoundry = 0.5
 upperBoundry = 2.5
-steps = 5 * 10**-3
+steps = 10**-4
+plotSteps = 5 * 10**-3
 g = x**4 + 2*x**5 + 3*x + 2
 #############################################
 # Methods Implementation
@@ -44,29 +49,68 @@ def getRandom( lowerBound , upperBound ):
     scaledValue = lowerBound + (value * (upperBound - lowerBound))
     return scaledValue
 
-def hillClimbing(expr, lowerBoundry , upperBoundry):
+def iterativeHillClimbing():
+    
+
+def hillClimbing(expr, lowerBoundry , upperBoundry , randomValue):
     w = Writer("results.txt")
     w.clearFile()
     # "expr" is our function
     dx = calculateDifferenciation(expr,1)
-    value = getRandom(lowerBoundry,upperBoundry)
+    #value = getRandom(lowerBoundry,upperBoundry)
+    value = randomValue
     slope = 1 if (dx.subs(x,value) > 0) else -1  
     amount =  dx.subs(x,value)
     derivationAmount = round(float(amount),1)
-    while (derivationAmount != 0.0 ):
+    while (derivationAmount != 0.0 and value >= lowerBoundry and value <= upperBoundry ):
         if( slope > 0 ):
             value -= steps
         else :
             value += steps
 
-        w.append(derivationAmount)
+        w.append(str(value) + " \t " + str(derivationAmount) )
         amount =  dx.subs(x,value)
         derivationAmount = round(float(amount),1)
 
     print("Done!")
     return value
 
+def plot(f,lowerBound,upperBound,target_x,step , plotTitle):
+    x_axis = []
+    y_axis = []
+    i = lowerBound 
+    while(i < upperBound):
+        x_axis.append(i)
+        y_axis.append(f.subs(x,i))
+        i += step
+    target_y = f.subs(x,target_x)
+   
+    # plotting the function points 
+    plt.plot(x_axis, y_axis, label = "function")   
+    # plotting the taget(local optima) points 
+    #plt.plot(target_x, target_y, label = "Local Minima") 
+    plt.scatter(target_x, target_y, color="black")
+    plt.plot([0, target_x], [target_y, target_y], linestyle="--", color="black")
+    plt.plot([target_x, target_x], [target_y,0], linestyle="--", color="black")
+    plt.annotate(r"$ x = " + str(round(target_x,2)) + "$", 
+             xy=(target_x, target_y), xytext=(-20, +80), annotation_clip=False, 
+             textcoords="offset points", fontsize=16, 
+             arrowprops=dict(arrowstyle="<-", connectionstyle="arc3,rad=.5"))
+    # naming the x axis 
+    plt.xlabel('x - axis') 
+    # naming the y axis 
+    plt.ylabel('y - axis') 
+    # giving a title to my graph 
+    plt.title(plotTitle) 
+    # show a legend on the plot 
+    plt.legend()     
+    # function to show the plot 
+    plt.show() 
+    
 ##############################################
+    
 print("Calculating:")   
-localOptima = hillClimbing(f,lowerBoundry,upperBoundry)
-print(localOptima)
+value = getRandom(lowerBoundry,upperBoundry)
+localX = localOptima = hillClimbing(f,lowerBoundry,upperBoundry , value )
+print(localX)
+plot(f,lowerBoundry,upperBoundry,localX,plotSteps,"Hill Climbing')
