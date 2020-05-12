0aafaae0e8347e2913e1576e6acb0884036ca55f
Hossein <hossein.dehghanipour1998@gmail.com>
2020-05-12 06:01:14 +0430

    debugged the code

--- 3 - Playing Cards/genetic.py
+++ 3 - Playing Cards/genetic.py
@@ -6,16 +6,26 @@ Created on Tue May 12 02:00:53 2020
 """
 
 #============ Imports ====================
-from random import random
+import random
+from math import *
 from ExternalLibraries.Writer import Writer as Writer
 #=========================================
 targetProduct = 360
 targetSum = 36
 parentsLength = 5 
+
+#=====================================================
+writer1 = Writer("Results/Parents.txt")
+write2 = Writer("Results/createBabies.txt")
+writer3 = Writer("Results/chooseBEstN.txt")
+
+writer1.clearFile();
+write2.clearFile()
+writer3.clearFile()
 #================ Extera Functions====================
 def getRandom( lowerBound , upperBound ):
     #returns a random number between the wanted boundries
-    value = random()
+    value = random.random()
     scaledValue = lowerBound + (value * (upperBound - lowerBound))
     return scaledValue
 
@@ -32,12 +42,14 @@ def sameTwoChildren(childOne , childTwo):
 def caculateFactors(child):
     summation = 0 
     production = 1  
+#    print("Len : " + str((child)))
     for chromosome in child:
         summation += chromosome
         production *= chromosome
     return summation,production
 
 def isSolution(children):
+    
     child_1_Summation , child_1_Production = caculateFactors(children[0])
     child_2_Summation , child_2_Production = caculateFactors(children[1])
     if(   
@@ -59,45 +71,77 @@ def createRandomParents(parentsLength):
         if ( not i in mother ):
             father.append(i)
             
-    print("Mother : " +  str(mother))
-    print("Father : " +  str(father))
+#    print("Mother : " +  str(mother))
+#    print("Father : " +  str(father))
     return (mother,father)    
 
-def getBestN(allChildren):
+def getBestN(population):
+
+    #writer.clearFile()
     minimumSum = targetSum
-    minimumProd = targetProduct
+    minimumProd = 1000
     sumChild = None
     prodChild = None
     
-    for child in allChildren :
+    
+    for child in population :       
         summ , prod = caculateFactors(child)
         util_Sum , util_prod = utility( summ , prod )
-        if ( util_Sum < minimumSum ):
+        if ( util_Sum <= minimumSum ):
+            minimumSum = util_Sum
             sumChild = child
-   
-    for child in allChildren :
+    
+    
+    for child in population :
         summ , prod = caculateFactors(child)
         util_Sum , util_prod = utility( summ , prod )
-        if ( util_prod < minimumProd and sameTwoChildren(child , sumChild ) == False) :
+        writer3.append(str(child) + "  \  " + str(util_prod))
+        if ( util_prod <= minimumProd and sameTwoChildren(child , sumChild ) == False) :
             prodChild = child
-        
+            minimumProd = util_prod     
     return sumChild , prodChild      
-        
        
 #================ Main Functions====================
     
 def utility(childSum , childProduct ):
-    return (targetProduct/childProduct , targetSum - childSum)
+    return ( float(childProduct/targetProduct) , abs(targetSum - childSum) )
 
-def crossOver( father , mother ):
-    # 6 types of crossing over :
+def crossOver( father , mother ):   
+
     children = []
-    children.append( father[0:3] + mother[3:5] )
-#    children.append( father[3:5] + mother[0:3] )
-#    children.append( father[2:5] + mother[0:2] )
-#    children.append( father[0:2] + mother[2:5] )
-    children.append( father[3:5] + mother[0:3] )
-#    children.append( father[0:3] + mother[3:5] )
+    child = []
+    write2.append("Mother : " + str(father) +  " Father : " + str(mother))
+    i = 0 
+    allowed = True
+    while i < 2 : 
+        child = []
+        while(True):
+            
+            if(len(child) < parentsLength ):
+                rndIndex = random.randint(-4,4)
+                g1 = father[rndIndex]
+                if(not g1 in child):
+                    child.append(g1)
+            
+            if(len(child) < parentsLength ):
+                rndIndex = random.randint(-4,4)
+                g2 = mother[rndIndex]
+                if(not g2 in child):
+                    child.append(g2)
+            else :
+                break
+            
+        if ( len(children) > 0 ):
+            if(sameTwoChildren(child,children[0]) == False ):
+                write2.append("Same Child Born")
+                i -= 1
+                allowed = False
+            else :
+                allowed = True
+        if ( allowed ):        
+            children.append(child)
+            write2.append(child)            
+        i += 1
     return children
 
 def mutate(child):    
@@ -107,27 +151,36 @@ def mutate(child):
         if ( not rndNumber in child ):
             child[rndIndex] = rndNumber
             break
-
+#==================================================
 
 def environmet():
+    population = []
     iterationNo = 0
-    writer = Writer("Results/children.txt")
-    writer.clearFile()
+    
     parents = createRandomParents(parentsLength)
+    
     mother = parents[0]
     father = parents[1]
-    writer.append(parents)
+    
+    population.append(mother)
+    population.append(father)
+    writer1.append(parents)
+    
     while True :
+        
+        print(iterationNo)
         newBornChildren = crossOver(mother,father)
-        mother , father = getBestN(newBornChildren)
-        newParents = (mother , father)
-        writer.append(newParents)
+        population.append(newBornChildren[0])
+        population.append(newBornChildren[1])
+        newParents = getBestN(population)
+        mother , father = newParents
+        population = [ mother , father]
+        writer1.append(newParents)
         if( isSolution(newParents) == True ):
             print("Found Solution")
             return newParents
         iterationNo += 1
-        if(iterationNo == 100):
-            return False
+
         
 
 def main():
