294b70206b6b923321385b33a73e266925a51a97
Hossein <hossein.dehghanipour1998@gmail.com>
2020-05-13 00:22:01 +0430

    Update genetic.py
    System halts after some test cases.
    80% of test cases reach the answer

--- 3 - Playing Cards/genetic.py
+++ 3 - Playing Cards/genetic.py
@@ -22,6 +22,8 @@ write2 = Writer("Results/createBabies.txt")
 writer3 = Writer("Results/chooseBEstN.txt")
 writer4 = Writer("Results/complete.txt")
 writer5 = Writer("Results/sortSome.txt")
+writer6 = Writer("Results/population.txt")
+writer7 = Writer("Results/Errors.txt")
 
 #================ Extera Functions====================
 def getRandom( lowerBound , upperBound ):
@@ -31,6 +33,7 @@ def getRandom( lowerBound , upperBound ):
     return scaledValue
 
 def sameTwoChildren(childOne , childTwo):
+    writer7.append("Same Two Children")
     appearance = 0 
     for item in childOne :
         if ( not item in childTwo ):
@@ -41,6 +44,7 @@ def sameTwoChildren(childOne , childTwo):
     return False
         
 def caculateFactors(child):
+    writer7.append("Calculate Factor")
     summation = 0 
     production = 1  
 #    print("Len : " + str((child)))
@@ -50,12 +54,13 @@ def caculateFactors(child):
     return summation,production
 
 def haveIntersection(children):
+    writer7.append("Hvae Intersection")
     for item in children[0]:
         if(item in children[1]):
             return True
     return False
 def isSolution(children):
-    
+    writer7.append("Is solution")
     child_1_Summation , child_1_Production = caculateFactors(children[0])
     child_2_Summation , child_2_Production = caculateFactors(children[1])
     if(   
@@ -64,6 +69,7 @@ def isSolution(children):
         return True
 
 def createRandomParents(parentsLength):
+    writer7.append("Create Random Parents")
     mother = []
     father = []
     i = 0 
@@ -82,6 +88,7 @@ def createRandomParents(parentsLength):
     return (mother,father)    
 
 def getBestN(population):
+    writer7.append("Get Best N")
     """ Approach 1 """
 #    minimumSum = targetSum
 #    minimumProd = 1000
@@ -108,25 +115,47 @@ def getBestN(population):
 #    writer3.append("{%s : sum = %s }  | { %s : mult = %s } |It : %s | " %(sumChild,minimumSum , prodChild ,minimumProd , iterationNo ))
 #    return sumChild , prodChild  
     """ Apperoach 2 """  
-    minimumSum = targetSum
+    minimumSum = 1000
     minimumProd = 1000
     sumChild = None
     prodChild = None
-    for child in population :   
+    for child in population : 
+        writer7.append("GBN - FOR 1")
         summ , prod = caculateFactors(child)
         util_prod , util_Sum = utility( summ , prod )
         if ( util_prod <= minimumProd) :
             prodChild = child
             minimumProd = util_prod
-    for child in population :       
+    for child in population :    
+        writer7.append("GBS - FOR 2")
         summ , prod = caculateFactors(child)
         util_prod , util_Sum = utility( summ , prod )
         if ( util_Sum <= minimumSum and haveIntersection((prodChild,child)) == False):
             minimumSum = util_Sum
             sumChild = child
-    return (sumChild,prodChild)
-
+    if (sumChild == None ):
+        writer7.append("GBN - IF")
+        for child in population :       
+            summ , prod = caculateFactors(child)
+            util_prod , util_Sum = utility( summ , prod )
+            if ( util_Sum <= minimumSum and countOverlap(child,prodChild) == 1):
+                minimumSum = util_Sum
+                sumChild = child
+        rnd = random.randint(0,100)
+        if ( rnd > mutationProbability ):
+            index = findIntersection(sumChild,prodChild)
+            sumChild = mutate(sumChild,True , index , prodChild)
+            
+            
+    return (prodChild,sumChild)
+def findIntersection(c1,c2):
+    writer7.append("FIND INTERSECTION")
+    for i in c1 :
+        if(i in c2):
+            return c2.index(i)
+    return -1
 def bubble_sort(nums,pop):
+    writer7.append("BUBBLE SORT")
     # We set swapped to True so the loop looks runs at least once
     swapped = True
     while swapped:
@@ -138,11 +167,15 @@ def bubble_sort(nums,pop):
                 pop[i], pop[i + 1] = pop[i + 1], pop[i]
                 # Set the flag to True so we'll loop again
                 swapped = True
-    writer5.append("================================================")           
-    writer5.append("IT : %s"%iterationNo)            
+               
+    writer5.append("IT : \n %s" % (iterationNo) )           
     writer5.append(nums)
+    writer5.append(pop)
+
+    
     
 def deportUnworthyPeople(population):
+    writer7.append("DEPORT ONWORTHY")
     worthyPop = []
     sum_nums = [] 
     prod_nums = []
@@ -157,8 +190,9 @@ def deportUnworthyPeople(population):
     for item in population:
         pop1.append(item)
         pop2.append(item)
-    
-    bubble_sort(sum_nums,pop1)    
+    writer5.append("====================== SUM==========================")
+    bubble_sort(sum_nums,pop1)  
+    writer5.append("====================== PROD ==========================")
     bubble_sort(prod_nums,pop2)
     i = 0 
     while i < populationSize/2 :
@@ -175,6 +209,7 @@ def deportUnworthyPeople(population):
     return worthyPop
 
 def countOverlap(li1 , li2):
+    writer7.append("COUNT OVERLAP")
     overlap = 0 
     for item in li1 :
         if(item in li2):
@@ -183,9 +218,11 @@ def countOverlap(li1 , li2):
 #================ Main Functions====================
     
 def utility(childSum , childProduct ):
+    writer7.append("UTILITY")
     return (abs(1 -  float(childProduct/targetProduct) ), abs(targetSum - childSum) )
 
-def crossOver( father , mother ):  
+def crossOver( father , mother ): 
+    writer7.append("CROSS OVER")
     """ Approach 1 """
 #    from random import shuffle
 #    shuffle(mother)
@@ -265,6 +302,7 @@ def crossOver( father , mother ):
     
     child1 = []
     while True :
+        writer7.append("CROSS OVER - WHILE 1")
         if(len(child1) < parentsLength ):
             rnd = random.randint(-4,4)
             if (not mother[rnd]  in child1):
@@ -282,13 +320,22 @@ def crossOver( father , mother ):
             
     return (child1,child2)
 
-def mutate( child ):    
-    while True :
-        rndNumber = random.randint(1,10)
-        rndIndex = random.randint(0,4)
-        if ( (not rndNumber in child) ):
-            child[rndIndex] = rndNumber
-            return child
+def mutate( child , status = False , index = 0 , brother = None): 
+    writer7.append("MUTATION")
+    if ( status == True):
+        while True :
+            writer7.append("IF - M ")
+            rndNumber = random.randint(1,10)
+            if(not rndNumber in brother):
+                child[index] = rndNumber
+    else :
+        writer7.append("ELSE - M")
+        while True :
+            rndNumber = random.randint(1,10)
+            rndIndex = random.randint(0,4)
+            if ( (not rndNumber in child) ):
+                child[rndIndex] = rndNumber
+                return child
 #    for item in range(1,11):
 #        if(not item in bro):
 #            child.append(item)
@@ -306,6 +353,7 @@ def environmet():
     writer5.clearFile()
     population = []
     
+    
 
     
     parents = createRandomParents(parentsLength)
@@ -316,15 +364,16 @@ def environmet():
     population.append(mother)
     population.append(father)
     writer1.append(parents)
-    
+
     while True :
-        
+        writer6.append("============================")
         print(iterationNo)
         newBornChildren = crossOver(mother,father)
         writer4.append("===========================")
         writer4.append("Newborn : " + str(newBornChildren))
         population.append(newBornChildren[0])
         population.append(newBornChildren[1])
+
         newParents = getBestN(population)
         mother , father = newParents
         
@@ -348,10 +397,17 @@ def environmet():
         
 
 def main():
-    print("Calculating...")
-    m , f = environmet()
-    print(str(m) + "\n" + str(f))
-    print("Done!")
+    results = []
+    for i in range (0,20) :           
+        print("Calculating...")
+        sol = environmet()
+        results.append(sol)
+        print(str(sol[0]) + "\n" + str(sol[1]))
+        print("Done!")
+    print("Totally Done")
+    print(len(results))
+    for item in results:
+        print(item)
         
 main()            
         
